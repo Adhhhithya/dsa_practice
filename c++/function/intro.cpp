49844f6d7b7fb9952234c182ad7ed210744cdb6c
Adhhhithya <adhithyaj06@gmail.com>
2025-08-02 06:05:29 +0530

    introduction to functions

---
+++ c++/function/intro.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+//void and parameterized function
+void PrintName(string name){
+    cout << " Hello. " << name << endl;
+}
+int main(){
+    string name ;
+    cin >> name;
+    PrintName(name);
+
+    string name2;
+    cin >> name2;
+    PrintName(name2);
+
+    return 0;
+
+}
