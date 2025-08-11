37688de0fc1880e5ffcedc3cd5272ca6d74de9cc
Adhhhithya <adhithyaj06@gmail.com>
2025-08-11 23:49:09 +0530

    hashing basics

---
+++ java/hashing/basic_hash.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class basic_hash {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. of array elements: ");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        //precompute hash array
+        int [] hash = new int[13];
+        for(int i=0;i<n;i++){
+            hash[arr[i]]+=1;
+        }
+
+        System.out.print("Enter no. of queries : ");
+        int q=sc.nextInt();
+        while(q>0){
+            System.out.println("Enter a number for query :");
+            int num = sc.nextInt();
+            System.out.println("The number "+num+" is present "+hash[num]+" times in the array");
+            q-=1;
+        }
+    }
+}
