444e8f0aa52bf44771cd925674a8c3c79e7301ed
Adhhhithya <adhithyaj06@gmail.com>
2025-10-29 00:13:19 +0530

    majority element (> N/3 times)

---
+++ tuf/java/arrays/hard/majority_element.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class majority_element {
+       public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        int cnt1=0,cnt2=0;
+        int el1=Integer.MIN_VALUE;
+        int el2=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(cnt1==0 && nums[i]!=el2){
+                cnt1=1;
+                el1=nums[i];
+            }
+            else if(cnt2==0 &&nums[i]!=el1){
+                cnt2=1;
+                el2=nums[i];
+            }
+            else if(el1==nums[i]){
+                cnt1++;
+            }else if(el2==nums[i]){
+                cnt2++;
+            }else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        List<Integer> ls = new ArrayList<>();
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==el1)cnt1++;
+            if(nums[i]==el2)cnt2++;
+        }
+        int mini = (int)(n/3)+1;
+        if(cnt1>=mini) ls.add(el1);
+        if(cnt2>=mini) ls.add(el2);
+
+        return ls;
+        
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array:");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter elements of array:");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        majority_element me = new majority_element();
+        List<Integer> res = me.majorityElement(arr);
+        System.out.println("Majority elements are:");
+        for(int ele: res){
+            System.out.print(ele + " ");
+        }
+        sc.close();
+    }
+    
+}
