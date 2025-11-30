e1ebb8d73f1f0b42337f60abda4deb08e9c21e2a
Adhhhithya <adhithyaj06@gmail.com>
2025-11-30 20:06:34 +0530

    Checking Palindrome in LinkedList

---
+++ tuf/java/linked-list/singly-linked-list/segregate_odd_even_nodes.java
@@ -0,0 +1,47 @@
+class Node{
+    int data;
+    Node next;
+    Node(int data1, Node next1){
+        this.data=data1;
+        this.next=next1;
+    }
+    Node(int data1){
+        this.data=data1;
+        this.next=null;
+    }
+}
+public class segregate_odd_even_nodes {
+    private static Node convertArr2LL(int arr[]){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i=1;i<arr.length;i++){
+            Node temp = new Node(arr[i]);
+            mover.next=temp;
+            mover = temp;
+        }
+        return head;
+    }
+    private static Node segregate_odd_even(Node head){
+        if(head==null || head.next==null) return head;
+        Node odd = head;
+        Node even = head.next;
+        Node evenHead = head.next;
+        while (even!=null && even.next!=null){ 
+            odd.next=odd.next.next;
+            even.next=even.next.next;
+
+            odd = odd.next;
+            even=even.next;
+        }
+        odd.next=evenHead;
+        return head;
+    }
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5,6};
+        Node head = convertArr2LL(arr);
+        segregate_odd_even(head);
+        for(Node temp = head;temp!=null;temp=temp.next){
+            System.out.print(temp.data+" ");
+        }
+    }
+}
