b18b5634ea3a618c5c71f71c6e4e3e9333d0bfce
Adhhhithya <adhithyaj06@gmail.com>
2026-01-10 00:39:48 +0530

    Reverse Words in a String

--- tuf/java/strings/easy/reverse_words_in_string.java
+++ tuf/java/strings/easy/reverse_words_in_string.java
@@ -1,28 +1,32 @@
 import java.util.*;
 public class reverse_words_in_string {
     public  String reverse_words(String s){
+        int n = s.length();
         List<String> words = new ArrayList<>();
-        StringBuilder word = new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch != ' '){
-                word.append(ch);
-            } else {
-                if(word.length() > 0){
-                    words.add(word.toString());
-                    word.setLength(0);
-                }
-            }
+        int start ,end;
+        int i=0;
+        while(i<n){
+            while(i<n && s.charAt(i)==' ')i++;
+            if(i>=n) break;
+            start =i;
+            while(i<n && s.charAt(i)!=' ')i++;
+            end=i-1;
+            String word = s.substring(start,end+1);
+            words.add(word);
         }
-        if(word.length()>0){
-            words.add(word.toString());
+        StringBuilder ans = new StringBuilder();
+        for(int j=words.size()-1;j>=0;j--){
+            ans.append(words.get(j));
+            if(j!=0) ans.append(' ');
         }
-        Collections.reverse(words);
-        return String.join(" ",words);
+        return ans.toString();
     }
     public static void main(String args[]){
         reverse_words_in_string obj = new reverse_words_in_string();
-        String s="Name my Say";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String s = sc.nextLine();
+        System.out.println("Reversed words string is:");
         System.out.println(obj.reverse_words(s));
     }
 }
